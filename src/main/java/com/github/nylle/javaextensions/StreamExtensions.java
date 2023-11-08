2c5c5c571481cd368b49258f3f4065fe3e1339a9
Jan <Nylle@users.noreply.github.com>
2023-11-09 00:19:08 +0100

    feat: improve partition

--- src/main/java/com/github/nylle/javaextensions/StreamExtensions.java
+++ src/main/java/com/github/nylle/javaextensions/StreamExtensions.java
@@ -19,59 +19,129 @@ import static lombok.AccessLevel.PRIVATE;
 public class StreamExtensions {
 
     /**
+     * Partitions {@code stream} into a {@link Stream} of lists with {@code size}.
+     * <p>
+     * For example, a stream with the elements
+     * [1 2 3 4 5]
+     * using a partition size of 2 will be partitioned into
+     * [[1 2] [3 4]]
+     * while items that do not make a complete partition are being dropped.
+     *
+     * @param stream the stream to partition
+     * @param size   the maximum size of each partition
+     * @param <T>    the type of elements in {@code stream}
+     * @return a stream of lists containing the elements of {@code stream}
+     */
+    public static <T> Stream<List<T>> partition(Stream<T> stream, int size) {
+        return partition(stream, size, size);
+    }
+
+    /**
+     * Partitions {@code stream} into a {@link Stream} of lists with {@code size} at offsets {@code step} apart.
+     * <p>
+     * For example, a stream with the elements
+     * [1 2 3 4 5 6 7 8 9]
+     * using a partition size of 2, a step of 4 will be partitioned into
+     * [[1 2] [5 6]]
+     * while items that do not make a complete partition are being dropped.
+     * <p>
+     * If {@code step} is smaller than {@code size}, elements are being re-used.
+     * For example, a stream with the elements
+     * [1 2 3 4 5]
+     * using a partition size of 3 and a step of 1 will be partitioned into
+     * [[1 2 3] [2 3 4] [3 4 5]]
+     *
+     * @param stream the stream to partition
+     * @param size   the maximum size of each partition
+     * @param step   the number of elements between the start of each partition
+     * @param <T>    the type of elements in {@code stream}
+     * @return a stream of lists containing the elements of {@code stream}
+     */
+    public static <T> Stream<List<T>> partition(Stream<T> stream, int size, int step) {
+        return partition(stream, size, step, null);
+    }
+
+    /**
+     * Partitions {@code stream} into a {@link Stream} of lists with {@code size} at offsets {@code step} apart
+     * padding with {@code pad} as necessary to complete the last partition.
+     * <p>
+     * For example, a stream with the elements
+     * [1 2 3 4 5 6 7 8 9]
+     * using a partition size of 2, a step of 4, and a pad of [0] will be partitioned into
+     * [[1 2] [5 6] [9 0]]
+     * <p>
+     * If {@code pad} contains not enough elements, the last partition may be shorter than {@code size}.
+     * For example, a stream with the elements
+     * [1 2 3 4 5 6 7]
+     * using a partition size of 3, a step of 3, and a pad of [0] will be partitioned into
+     * [[1 2 3] [4 5 6] [7 0]]
+     * <p>
+     * If {@code pad} contains too many elements, they will be discarded beyond {@code size}.
+     * For example, a stream with the elements
+     * [1 2 3 4 5 6 7]
+     * using a partition size of 3, a step of 3, and a pad of [0 0 0] will be partitioned into
+     * [[1 2 3] [4 5 6] [7 0 0]]
+     * <p>
+     * If {@code step} is smaller than {@code size}, elements are being re-used.
+     * For example, a stream with the elements
+     * [1 2 3 4 5]
+     * using a partition size of 3 and a step of 1 will be partitioned into
+     * [[1 2 3] [2 3 4] [3 4 5]]
+     *
+     * @param stream the stream to partition
+     * @param size   the maximum size of each partition
+     * @param step   the number of elements between the start of each partition
+     * @param <T>    the type of elements in {@code stream}
+     * @return a stream of lists containing the elements of {@code stream}
+     */
+    public static <T> Stream<List<T>> partition(Stream<T> stream, int size, int step, List<T> pad) {
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(new BatchIterator<>(stream.iterator(), size, step, pad), ORDERED), false).filter(x -> !x.isEmpty());
+    }
+
+    /**
      * Creates a lazy sliding window with {@code size} for the elements in {@code stream}.
      * <p>
      * No matter how large the window is, it will always contain only one new element, which means that the "overlap" is always {@code size}-1.
      * <p>
      * For example, a stream with the elements
-     *   [1 2 3 4 5]
+     * [1 2 3 4 5]
      * and window-size of 3 will be partitioned into
-     *   [[1 2 3] [2 3 4] [3 4 5]]
+     * [[1 2 3] [2 3 4] [3 4 5]]
      *
      * @param stream the stream to create the sliding window over
-     * @param size the size of the sliding window
+     * @param size   the size of the sliding window
+     * @param <T>    the type of elements in {@code stream}
      * @return a stream of lists with {@code size}
-     * @param <T> the type of elements in {@code stream}
      */
     public static <T> Stream<List<T>> slidingWindow(Stream<T> stream, int size) {
-        Queue<T> queue = new LinkedList<>();
-        return stream
-                .dropWhile(item -> queue.size() < size - 1 && queue.add(item))
-                .map(item -> {
-                    queue.add(item);
-                    var result = queue.stream().toList();
-                    queue.remove();
-                    return result;
-                });
+        return partition(stream, size, 1);
     }
 
-    /**
-     * Partitions {@code stream} into a {@link Stream} of lists with {@code size}.
-     * <p>
-     * For example, a stream with the elements
-     *   [1 2 3 4 5]
-     * using a partition size of 2 will be transformed into
-     *   [[1 2] [3 4] [5]]
-     *
-     * @param stream the stream to partition
-     * @param size the maximum size of each partition
-     * @return a stream of lists containing the elements of {@code stream}
-     * @param <T> the type of elements in {@code stream}
-     */
-    public static <T> Stream<List<T>> partition(Stream<T> stream, int size) {
-        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(new BatchIterator<>(stream.iterator(), size), ORDERED), false);
+    private static Stream<Integer> range(int exclusive) {
+        return Stream.iterate(0, i -> i + 1).limit(exclusive);
     }
 
     @RequiredArgsConstructor
     private static class BatchIterator<T> implements Iterator<List<T>> {
         private final Iterator<T> iterator;
         private final int batchSize;
-        private List<T> currentBatch;
+        private final int step;
+        private final List<T> pad;
+        private final Queue<T> queued = new LinkedList<>();
+        private List<T> discarded = new ArrayList<>();
 
         @Override
         public List<T> next() {
-            prepareNextBatch();
-            return currentBatch;
+            prepareNextPartition();
+            dropItems(step - batchSize);
+
+            var partition = queued.stream().limit(batchSize).toList();
+
+            if (pad == null && partition.size() < batchSize) {
+                return List.of();
+            }
+
+            return partition;
         }
 
         @Override
@@ -79,11 +149,38 @@ public class StreamExtensions {
             return iterator.hasNext();
         }
 
-        private void prepareNextBatch() {
-            currentBatch = new ArrayList<>(batchSize);
-            while (iterator.hasNext() && currentBatch.size() < batchSize) {
-                currentBatch.add(iterator.next());
+        private void prepareNextPartition() {
+            cleanUpQueue();
+            while (iterator.hasNext() && queued.size() < batchSize) {
+                queued.add(iterator.next());
             }
+            padQueue();
+        }
+
+        private void padQueue() {
+            if (queued.size() < batchSize && pad != null) {
+                queued.addAll(pad);
+            }
+        }
+
+        private void cleanUpQueue() {
+            if (queued.isEmpty()) {
+                return;
+            }
+
+            if (step > batchSize) {
+                queued.clear();
+                return;
+            }
+
+            range(step).forEach(i -> queued.remove());
+        }
+
+        private void dropItems(int count) {
+            while (iterator.hasNext() && discarded.size() < count) {
+                discarded.add(iterator.next());
+            }
+            discarded = new ArrayList<>();
         }
     }
 }
--- src/test/java/com/github/nylle/javaextensions/StreamExtensionsTest.java
+++ src/test/java/com/github/nylle/javaextensions/StreamExtensionsTest.java
@@ -1,6 +1,7 @@
 package com.github.nylle.javaextensions;
 
 import lombok.experimental.ExtensionMethod;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -16,15 +17,66 @@ class StreamExtensionsTest {
     class Partition {
 
         @Test
-        void isLazy() {
-            var infiniteStream = Stream.iterate(0, i -> i + 1);
+        @DisplayName("returns a lazy stream of lists of n items")
+        void returnsALazyStreamOfListsOfNItems() {
+            assertThat(Stream.iterate(0, i -> i + 1).partition(2).skip(1).limit(3).toList()).containsExactly(
+                    List.of(2, 3),
+                    List.of(4, 5),
+                    List.of(6, 7));
+        }
 
-            var actual = infiniteStream.partition(3).skip(1).limit(3).toList();
+        @Test
+        @DisplayName("drops items that make not a complete partition")
+        void dropsItemsThatMakeNotACompletePartition() {
+            assertThat(Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).partition(4).toList()).containsExactly(
+                    List.of(0, 1, 2, 3),
+                    List.of(4, 5, 6, 7));
+        }
 
-            assertThat(actual).containsExactly(
-                    List.of(3, 4, 5),
-                    List.of(6, 7, 8),
-                    List.of(9, 10, 11));
+        @Test
+        @DisplayName("uses step to select the starting point for each partition")
+        void usesStepToSelectTheStartingPointForEachPartition() {
+            assertThat(Stream.iterate(0, i -> i + 1).partition(4, 6).limit(3).toList()).containsExactly(
+                    List.of(0, 1, 2, 3),
+                    List.of(6, 7, 8, 9),
+                    List.of(12, 13, 14, 15));
+        }
+
+        @Test
+        @DisplayName("re-uses items if step is smaller than partition size")
+        void reUsesItemsIfStepIsSmallerThanPartitionSize() {
+            assertThat(Stream.iterate(0, i -> i + 1).partition(3, 2).limit(4).toList()).containsExactly(
+                    List.of(0, 1, 2),
+                    List.of(2, 3, 4),
+                    List.of(4, 5, 6),
+                    List.of(6, 7, 8));
+        }
+
+        @Test
+        @DisplayName("pads last partition if not complete")
+        void padsLastPartitionIfNotComplete() {
+            assertThat(Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).partition(3, 4, List.of(0)).toList()).containsExactly(
+                    List.of(0, 1, 2),
+                    List.of(4, 5, 6),
+                    List.of(8, 9, 0));
+        }
+
+        @Test
+        @DisplayName("returns a shorter last partition if the padding is not long enough")
+        void returnsAShorterLastPartitionIfThePaddingIsNotLongEnough() {
+            assertThat(Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).partition(3, 4, List.of()).toList()).containsExactly(
+                    List.of(0, 1, 2),
+                    List.of(4, 5, 6),
+                    List.of(8, 9));
+        }
+
+        @Test
+        @DisplayName("pads last partition if not complete discarding overflow")
+        void padsLastPartitionIfNotCompleteDiscardingOverflow() {
+            assertThat(Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).partition(3, 4, List.of(0, 0, 0, 0)).toList()).containsExactly(
+                    List.of(0, 1, 2),
+                    List.of(4, 5, 6),
+                    List.of(8, 9, 0));
         }
     }
 
